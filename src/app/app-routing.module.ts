0f6499617465e20255a8707587f96a27c6ebca93
Ignacio <nachogrignola@gmail.com>
2021-09-28 15:36:16 +0200

    The home view was created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ResultComponent } from './pages/result/result.component';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
   },
   {
-    path: '',
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path:'result',
+    component: ResultComponent
+  },
+  {
+    path: '**',
     redirectTo: 'home',
     pathMatch: 'full'
   },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,20 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ResultComponent } from './pages/result/result.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
+  declarations: 
+  [ AppComponent,
+    HomeComponent,
+    ResultComponent
+  ],
+  entryComponents: [
+    HomeComponent,
+    ResultComponent
+  ],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
